cb71bb21c3ece9041a5116b4327de7083a99d0f4
tylergersbach <34660409+tylergersbach@users.noreply.github.com>
2017-12-19 13:08:54 -0500

    Cleaned up code, moved out redundant classes and added their function to other classes where appropriate.

--- javaproject/test/Mock.java
+++
@@ -1,6 +0,0 @@
-
-public class Mock{
-	public static void show(String out){
-		System.err.println(Thread.currentThread().getName()+":"+out);
-	}
-}
--- javaproject/test/MockI.java
+++
@@ -1,4 +0,0 @@
-
-public class MockI{
-	public static void show(String out){System.err.println(out);}
-}
